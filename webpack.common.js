6c4271e5343e94542727419095a98c768617ef52
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-09 17:28:14 -0500

    Set up file-loader and html-loader
    -We set up this two loaders to move our files from
    src to dist, excluding the template.html, which the
    html-webpack-plugin is taking care of.
    -We also added clean-webpack-plugin to clean our dist
    directory every time we run 'npm run build'.

--- webpack.common.js
+++ webpack.common.js
@@ -17,7 +17,24 @@ module.exports = {
     {
         test: /\.(png|jpg|gif)$/i,
         use:'url-loader',
-    }
+    },
+    {
+       test: /\.html$/i,
+        exclude: /template\.html/,
+       loader: 'html-loader',
+     },
+     {
+       test: /\.(svg|png|jpe?g|gif)$/i,
+       use: [
+         {
+           loader: 'file-loader',
+           options:{
+             name:"[name].[hash].[ext]",
+             outputPath: "imgs"
+           }
+         },
+       ],
+     },
   ]
   }
 };
