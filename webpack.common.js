83ae46d3b3a16ef2fb51e2dda4db88b6183e340c
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-09 12:36:02 -0500

    Split Dev & Prod

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,11 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
 
 module.exports = {
-  mode: "development",
   devtool: false,
   entry: "./src/index.js",
-  output: {
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, 'dist')
-  },
   plugins:[new HtmlWebpackPlugin({
-    template:'./src/template.html'
+    template:'./src/template.html',
+    minify: false
   })],
   module:{
     rules:[
