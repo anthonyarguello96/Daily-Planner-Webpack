834be87021c3790a288e930cc480e43596916f8a
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-12 15:35:56 -0500

    Minify CSS & JS

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,10 @@ const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-const MiniCssExtractPlugin =require("mini-css-extract-plugin")
+const MiniCssExtractPlugin =require("mini-css-extract-plugin");
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = merge(common, {
   mode: "production",
   devtool: false,
@@ -11,6 +14,12 @@ module.exports = merge(common, {
     filename: "main.[contenthash].js",
     path: path.resolve(__dirname, 'dist')
   },
+ optimization:{
+   minimizer: [
+     new OptimizeCssAssetsPlugin(),
+     new TerserPlugin()
+   ]
+ },
   plugins: [
   new MiniCssExtractPlugin({filename: "[name].[contenthash].css"}),
   new CleanWebpackPlugin()
