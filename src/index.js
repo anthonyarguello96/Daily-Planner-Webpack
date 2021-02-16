90b4f1b74d3727a4c09a9b0f5cb98e6805908662
anthonyarguello96 <anthonyarguello96@gmail.com>
2021-02-15 19:21:11 -0500

    Install FullCalendar dependecies

--- src/index.js
+++ src/index.js
@@ -9,6 +9,44 @@ const parallax = document.getElementsByClassName('img-div');
 const spread = [...parallax];
 const prioritiesContainer = document.getElementById('priorities-box');
 const prioritiesList = document.getElementById('priorities-list');
+import { Calendar } from '@fullcalendar/core';
+import interactionPlugin from '@fullcalendar/interaction';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import listPlugin from '@fullcalendar/list';
+import './main.css';
+
+document.addEventListener('DOMContentLoaded', function() {
+  var calendarEl = document.getElementById('calendar');
+
+  var calendar = new Calendar(calendarEl, {
+    plugins: [ interactionPlugin, dayGridPlugin, timeGridPlugin, listPlugin ],
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialDate: '2018-01-12',
+    navLinks: true, // can click day/week names to navigate views
+    editable: true,
+    dayMaxEvents: true, // allow "more" link when too many events
+    events: [
+      {
+        title: 'All Day Event',
+        start: '2021-02-15',
+      },
+      {
+        title: 'Click for Google',
+        url: 'http://google.com/',
+        start: '2021-02-16'
+      }
+    ]
+  });
+
+  calendar.render();
+});
+
+
 // const entries = document.getElementsByClassName('item-input');
 // console.log(entries);
 // Add to-do
@@ -30,5 +68,3 @@ inputField.addEventListener('keyup', function(e) {
 window.addEventListener('scroll', function() {
   lib.parallaxEffect(spread);
 });
-
-console.log('hello');
